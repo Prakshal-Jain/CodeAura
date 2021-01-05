22ac7b051430cf1a7a4fbe8fdc70a9befa0e714c
Harjot Barn <harjotba@buffalo.edu>
2021-01-05 10:59:15 -0500

    static files

---
+++ gulpfile.babel.js
@@ -0,0 +1,104 @@
+'use strict';
+
+import browserSync from 'browser-sync';
+import del from 'del';
+import path from 'path';
+import gulp from 'gulp';
+import named from 'vinyl-named';
+import yargs from 'yargs';
+
+
+var sass = require('gulp-dart-sass');
+const sourceMaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const cleanCss = require('gulp-clean-css');
+const gulpif = require('gulp-if');
+const imagemin = require('gulp-imagemin');
+
+const PRODUCTION = !!yargs.argv.p;
+const _STATIC = path.resolve('static');
+const _NODE_MODULES = path.resolve('node_modules');
+
+//  paths
+const PATHS = {
+  src: {
+    base: path.join(_STATIC, 'src'),
+    images: path.join(_STATIC, 'src', 'img'),
+    scripts: path.join(_STATIC, 'src', 'js'),
+    styles: path.join(_STATIC, 'src', 'scss'),
+  },
+  dest: {
+    base: path.join(_STATIC, 'dist'),
+    images: path.join(_STATIC, 'dist', 'img'),
+    scripts: path.join(_STATIC, 'dist', 'js'),
+    styles: path.join(_STATIC, 'dist', 'css'),
+  },
+};
+
+
+function browserSyncServer(c) {
+  browserSync.init({ proxy: 'localhost:8000' });
+  c();
+}
+
+function cleanDist() {
+  return del(path.join(PATHS.dest.base, '**'));
+}
+
+function sassCompile() {
+  return gulp
+    .src(path.join(PATHS.src.styles, '**', '*'))
+    .pipe(gulpif(!PRODUCTION, sourceMaps.init()))
+    .pipe(sass({ includePaths: PATHS.lib.styles }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulpif(PRODUCTION, cleanCss({ compatibility: 'ie9' })))
+    .pipe(gulpif(!PRODUCTION, sourceMaps.write()))
+    .pipe(gulp.dest(PATHS.dest.styles))
+    .pipe(browserSync.stream());
+}
+
+function images() {
+  return gulp
+    .src(path.join(PATHS.src.images, '**', '*'))
+    .pipe(imagemin())
+    .pipe(gulp.dest(PATHS.dest.images));
+}
+
+function scripts() {
+  return (
+    gulp
+      .src(path.join(PATHS.src.scripts, '**', '*.js'))
+      .pipe(named())
+      .pipe(gulpif(!PRODUCTION, sourceMaps.init()))
+      .pipe(webpackStream(webpackConfig, webpack))
+      .pipe(gulpif(PRODUCTION, uglify()))
+      .pipe(gulpif(!PRODUCTION, sourceMaps.write()))
+      .pipe(gulp.dest(PATHS.dest.scripts))
+  );
+}
+
+function others(c) {
+  // console.log('others');
+  c();
+}
+
+function watchChanges() {
+  gulp
+    .watch(path.join(path.resolve('templates'), '**', '*'))
+    .on('all', browserSync.reload);
+  gulp
+    .watch(path.join(PATHS.src.images, '**', '*'))
+    .on('all', gulp.series(images, browserSync.reload));
+  gulp
+    .watch(path.join(PATHS.src.scripts, '**', '*'))
+    .on('all', gulp.series(scripts, browserSync.reload));
+  gulp.watch(path.join(PATHS.src.styles, '**', '*')).on('all', sassCompile);
+}
+
+gulp.task(
+  'build',
+  gulp.series(cleanDist, gulp.parallel(scripts, sassCompile, others, images))
+);
+
+gulp.task('watch', gulp.series('build', browserSyncServer, watchChanges));
