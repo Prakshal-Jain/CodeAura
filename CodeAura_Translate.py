341184469eaac65910527686a9e805aa53d7ec8a
Prakshal Jain <61107289+Prakshal-Jain@users.noreply.github.com>
2021-01-16 16:02:16 -0500

    Update CodeAura_Translate.py

--- CodeAura_Translate.py
+++ CodeAura_Translate.py
@@ -11,36 +11,40 @@
 
 import http.client
 
-def Translate(txtFromUser : str):
-	conn = http.client.HTTPSConnection("microsoft-translator-text.p.rapidapi.com")
 
-	payload = "[\r{\r\"Text\": \"I would really like to drive your car around the block a few times.\"\r}\r]"
+def Translate(destinationLang: str, userStr: str):
+    conn = http.client.HTTPSConnection("microsoft-translator-text.p.rapidapi.com")
 
-	headers = {
-    	'content-type': "application/json",
-    	'x-rapidapi-key': "d15cb7a0bbmsh95063875d7c71f4p14d8ffjsn696853778b90",
-    	'x-rapidapi-host': "microsoft-translator-text.p.rapidapi.com"
-	}
+    payload = "[\r{\r\"Text\": \""+userStr+"\"\r}\r]"
 
-	#input text from user goes into the request
-	conn.request("POST", "/translate?to=***txtFromUser***&api-version=3.0&profanityAction=NoAction&textType=plain", payload, headers)
-
-	res = conn.getresponse()
-	data = res.read()
-
-	#print(data.decode("utf-8"))
-
-def getLanguages():
-	conn = http.client.HTTPSConnection("microsoft-translator-text.p.rapidapi.com")
-
-	headers = {
-    	'x-rapidapi-key': "d15cb7a0bbmsh95063875d7c71f4p14d8ffjsn696853778b90",
-    	'x-rapidapi-host': "microsoft-translator-text.p.rapidapi.com"
+    headers = {
+        'content-type': "application/json",
+        'x-rapidapi-key': "d15cb7a0bbmsh95063875d7c71f4p14d8ffjsn696853778b90",
+        'x-rapidapi-host': "microsoft-translator-text.p.rapidapi.com"
     }
 
-	conn.request("GET", "/languages?api-version=3.0", headers=headers)
+    # input text from user goes into the request
+    conn.request("POST", "/translate?to="+destinationLang+"&api-version=3.0&profanityAction=NoAction&textType=plain",
+                 payload, headers)
 
-	res = conn.getresponse()
-	data = res.read()
+    res = conn.getresponse()
+    data = res.read()
 
-	#print(data.decode("utf-8"))
+    # This is a JSON data. Use json.load to get the contents.
+    return data.decode("utf-8")
+
+def getLanguages():
+    conn = http.client.HTTPSConnection("microsoft-translator-text.p.rapidapi.com")
+
+    headers = {
+        'x-rapidapi-key': "d15cb7a0bbmsh95063875d7c71f4p14d8ffjsn696853778b90",
+        'x-rapidapi-host': "microsoft-translator-text.p.rapidapi.com"
+    }
+
+    conn.request("GET", "/languages?api-version=3.0", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+    
+    # This is a JSON data. Use json.load to get the contents.
+    return data.decode("utf-8")
