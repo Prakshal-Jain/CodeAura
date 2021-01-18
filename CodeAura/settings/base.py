2c95f4a0f165045841946e41dcd581d41a7bcbe0
Prakshal <prakshal@buffalo.edu>
2021-01-18 05:56:10 -0500

    changes

--- CodeAura/settings/base.py
+++ CodeAura/settings/base.py
@@ -87,7 +87,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "..", "..", "static", "dist")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static", "dist")]
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "..", "..", "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
